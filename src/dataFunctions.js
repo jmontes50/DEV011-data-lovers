76981e03b4db30316cbcdc5a47277dba04ff7742
Erika Valdes <envm92@gmail.com>
2023-10-06 08:55:03 -0700

    Exportar funcion, se probo ordenamiento

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,8 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return [];
-};
+export const sortList = (pokemonList, order = 'asc') => {
+  return pokemonList.sort((a,b) => {
+    const isAsc = order === 'asc';
+    if ( a.name < b.name) { return isAsc ? -1 : 1; }
+    else if ( b.name < a.name ) { return isAsc ? 1 : -1 }
+    return 0;
+  });
+}
